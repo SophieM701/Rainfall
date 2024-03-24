0d92ee0534ef3a98d98629a0b4c2b82118a0bd07
Sophie Madi <sophieam701@gmail.com>
2024-03-24 14:53:06 -0500

    Animation Cleanup

--- Objects/Characters/obj_player.gd
+++ Objects/Characters/obj_player.gd
@@ -14,6 +14,13 @@ var coyote = 0
 # Flipped?
 var flip = false
 
+# For S&S
+var floor_impact = false
+var stretch_y = 1.0
+var stretch_x = 1.0
+
+var crouch = 0
+
 
 # Handle movement, including player input
 func _physics_process(delta):
@@ -30,20 +37,35 @@ func _physics_process(delta):
 	# Handle jump
 	if Input.is_action_just_pressed("Jump") and (coyote > 0):
 		velocity.y = JUMP_VELOCITY
+		# Stretch
+		stretch_y = 1.4
+		stretch_x = 0.8
+		
 
 	# Get input direction 
 	var direction = Input.get_axis("Left", "Right")
+	velocity.x = direction * SPEED
+	
+	# Crouch
+	#if( Input.is_action_pressed("Down") ):
+	#	if( crouch < 2 ):
+	#		crouch += 1
+	#elif( crouch > 0 ):
+	#	crouch -= 1
+		
 	
 	# If there is input and player is below move speed, add portion of move speed
-	if( abs(velocity.x + direction * SPEED * 0.4) < abs(SPEED) ):
-		velocity.x += direction * SPEED * 0.4
+	#if( abs(velocity.x + direction * SPEED * 0.4) < abs(SPEED) ):
+	#	velocity.x += direction * SPEED * 0.4
 	
 	# Friction on ground
-	if ( self.is_on_floor && (direction == 0) ):
-		velocity.x -= direction * SPEED * 0.4 
+	#if ( self.is_on_floor && (direction == 0) ):
+	#	velocity.x -= direction * SPEED * 0.4 
 	
 	
+	# Animation Things
 	
+	# Flip sprite based on direction
 	if(direction < 0):
 		$Sprite.flip_h = true
 		flip = true
@@ -51,9 +73,47 @@ func _physics_process(delta):
 		$Sprite.flip_h = false
 		flip = false
 		
+	# Landing
+	if( coyote > 0 && !floor_impact):
+		floor_impact = true
+		stretch_x = 1.3
+		stretch_y = 0.7
+	
+	if( coyote <= 0 ):
+		floor_impact = false
+		
+	# Update S&S:
+	$Sprite.scale.x = stretch_x
+	$Sprite.scale.y = stretch_y
+	stretch_x = ((stretch_x - 1)*0.85) + 1
+	stretch_y = ((stretch_y - 1)*0.85) + 1
+		
+		
+	# Idle animation
+	if( is_on_floor() ):
+		$Sprite.play("idle")
+		
+	# Crouch animation
+	if( crouch == 1 ):
+		$Sprite.play("crouchA")
+	if( crouch >= 2 ):
+		$Sprite.play("crouchB")
+	
+	
+	# Jump and fall
+	if( !is_on_floor() && velocity.y < 0 ):
+		$Sprite.play("jump")
+		
+	if( !is_on_floor() && velocity.y > 0 && velocity.y <= 20):
+		$Sprite.play("idle")
+		
+	if( !is_on_floor() && velocity.y > 20 ):
+		$Sprite.play("fall")
+		
+	# Jump Squash and Stretch
 	
 	# Terminal Velocity
-	
-	
+
+
 
 	move_and_slide()
